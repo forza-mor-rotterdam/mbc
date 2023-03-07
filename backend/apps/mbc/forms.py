501c73d6286016bd10331e89a71c4196938292f1
Remco Hoff <remco@code010.nl>
2023-03-07 15:36:09 +0100

    Moving the form to django

--- backend/apps/mbc/forms.py
+++ backend/apps/mbc/forms.py
@@ -1,4 +1,120 @@
 from django import forms
 
 class MeldingAanmakenForm(forms.Form):
-    pass
+    fieldsets = (
+        {
+            "titel": "Fieldset naam",
+            "icon": "Fieldset icon",
+            "fields": (
+            "begraafplaats",
+            "naam_melder",
+            )
+        },
+        {
+        
+        }
+    )
+
+    begraafplaats = forms.ChoiceField(
+        widget=forms.Select(
+            attrs={
+                "class": "form-select",
+            }
+        ),
+        label="Begraafplaats",
+        choices=(
+            ("begraafplaats_crooswijk", "Begraafplaats Crooswijk"),
+            ("begraafplaats_hoek_van_holland", "Begraafplaats Hoek van Holland"),
+            ("begraafplaats_hofwijk", "Begraafplaats en crematorium Hofwijk"),
+            ("begraafplaats_oude_land", "Begraafplaats Oudeland, Hoogvliet"),
+            ("begraafplaats_oud_hoogvliet", "Begraafplaats Oud-Hoogvliet"),
+            ("begraafplaats_oud_overschie", "Begraafplaats Oud-Overschie"),
+            ("begraafplaats_oud_pernis", "Begraafplaats Oud-Pernis"),
+            ("begraafplaats_oud_schiebroek", "Begraafplaats Oud-Schiebroek"),
+            ("begraafplaats_pernis", "Begraafplaats Pernis"),
+            ("begraafplaats_rozenburg", "Begraafplaats Rozenburg"),
+            ("begraafplaats_zuiderbegraafplaats", "De Zuiderbegraafplaats"),
+        ),
+        required=True,
+
+    )
+
+    grafnummer = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+        label="Grafnummer of colombarium",
+        required=False
+    )
+
+    vak = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+        label="Vak",
+        required=False
+    )
+
+    naam_overledene = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+        label="Naam overledene",
+        required=False
+    )
+
+    categorie = forms.ChoiceField(
+        widget=forms.CheckboxSelectMultiple(
+            attrs={
+                "class": "form-check-input",
+                 "data-action": "request#toggleInputOtherCategory",
+            }
+        ),
+        label="Categorie",
+        choices=(
+            ("categorie_verzakking_eigen_graf", "Verzakking eigen graf"),
+            ("categorie_verzakking_algemeen", "Verzakking algemeen"),
+            ("categorie_snoeien", "Snoeien"),
+            ("categorie_beplanting", "Beplanting"),
+            ("categorie_schoonmaken", "Schoonmaken"),
+            ("categorie_verdwenen_materiaal", "Verdwenen materiaal"),
+            ("categorie_gaten", "Gaten"),
+            ("categorie_wespennest", "Wespennest"),
+            ("categorie_konijnen", "Konijnen"),
+            ("categorie_muizen", "Muizen"),
+            ("categorie_zerk_reinigen", "Zerk reinigen"),
+            ("categorie_overig", "Overig"),
+        ),
+        required=True
+    )
+
+    categorie_omschrijving = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "data-request-target": "categoryDescription",
+            }
+        ),
+        label="Overig",
+        required=True
+    )
+
+    toelichting = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "rows": "4",
+            }
+        ),
+        label="Toelichting",
+        required=False
+    )
+
+    # def as_fieldsets(self):
+
