d81117b3790376a0d43fcafa5f018cfd129b93f0
Maurice Guikema <mguikema@gmail.com>
2023-03-16 13:46:07 +0100

    send to bc

--- backend/apps/mbc/views.py
+++ backend/apps/mbc/views.py
@@ -1,5 +1,6 @@
 import json
 
+from apps.mbc.constanten import BEGRAAFPLAATS_EMAIL_ADRES
 from apps.mbc.forms import MeldingAanmakenForm
 from django.conf import settings
 from django.core.mail import send_mail
@@ -41,7 +42,13 @@ def melding_aanmaken(request):
         print(form.errors)
         dirty_fields_list = json.loads(form.data.get("dirty_fields", "[]"))
         if is_valid:
-            send_to = ["maurice@tiltshift.nl"]
+            send_to = []
+            if BEGRAAFPLAATS_EMAIL_ADRES.get(form.cleaned_data.get("begraafplaats")):
+                send_to.append(
+                    BEGRAAFPLAATS_EMAIL_ADRES.get(
+                        form.cleaned_data.get("begraafplaats")
+                    )
+                )
             if form.cleaned_data.get("email_melder"):
                 send_to.append(form.cleaned_data.get("email_melder"))
             send_mail(
