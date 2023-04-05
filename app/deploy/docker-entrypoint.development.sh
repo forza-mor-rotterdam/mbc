8860a429eb5630394c1af190e2cc7925edb3b225
Maurice Guikema <mguikema@gmail.com>
2023-04-05 10:22:46 +0200

    cleanup

--- app/deploy/docker-entrypoint.development.sh
+++ app/deploy/docker-entrypoint.development.sh
@@ -3,8 +3,7 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x
 
-echo Collecting static files
-python manage.py collectstatic --no-input
+rm -rf /static/*
 
 echo Test cache
 python manage.py test_cache
