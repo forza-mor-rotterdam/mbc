b30d213be6d3d307e1a0f486c4d754af1299a4a1
Maurice Guikema <mguikema@gmail.com>
2023-06-26 18:07:47 +0200

    mail attach with cid

--- app/apps/services/mail.py
+++ app/apps/services/mail.py
@@ -1,18 +1,100 @@
 import base64
 import logging
+import os.path
+import re
 
+import magic
 from apps.mbc.models import Begraafplaats, Categorie
 from apps.services.meldingen import MeldingenService
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
+from django.core.files.storage import default_storage
+from django.core.mail import EmailMultiAlternatives, SafeMIMEMultipart
 from django.template.loader import get_template
 
 logger = logging.getLogger(__name__)
 
 
+class EmailMultiRelated(EmailMultiAlternatives):
+    """
+    A version of EmailMessage that makes it easy to send multipart/related
+    messages. For example, including text and HTML versions with inline images.
+
+    @see https://djangosnippets.org/snippets/2215/
+    """
+
+    related_subtype = "related"
+
+    def __init__(self, *args, **kwargs):
+        # self.related_ids = []
+        self.related_attachments = []
+        return super().__init__(*args, **kwargs)
+
+    def attach_related(self, filename, content, mimetype):
+        self.related_attachments.append((filename, content, mimetype))
+
+    def attach_related_file(self, path):
+        """Attaches a file from the filesystem."""
+        filename = os.path.basename(path)
+        content = open(path, "rb").read()
+        mime = magic.Magic(mime=True)
+        mimetype = mime.from_file(path)
+        self.attach_related(filename, content, mimetype)
+
+    def _create_message(self, msg):
+        return self._create_attachments(
+            self._create_related_attachments(self._create_alternatives(msg))
+        )
+
+    def _create_alternatives(self, msg):
+        for i, (content, mimetype) in enumerate(self.alternatives):
+            if mimetype == "text/html":
+                for related_attachment in self.related_attachments:
+                    filename, _, _ = related_attachment
+                    content = re.sub(
+                        r"(?<!cid:)%s" % re.escape(filename),
+                        "cid:%s" % filename,
+                        content,
+                    )
+                self.alternatives[i] = (content, mimetype)
+
+        return super()._create_alternatives(msg)
+
+    def _create_related_attachments(self, msg):
+        encoding = self.encoding or settings.DEFAULT_CHARSET
+        if self.related_attachments:
+            body_msg = msg
+            msg = SafeMIMEMultipart(_subtype=self.related_subtype, encoding=encoding)
+            if self.body:
+                msg.attach(body_msg)
+            for related_attachment in self.related_attachments:
+                msg.attach(self._create_related_attachment(*related_attachment))
+        return msg
+
+    def _create_related_attachment(self, filename, content, mimetype=None):
+        """
+        Convert the filename, content, mimetype triple into a MIME attachment
+        object. Adjust headers to use Content-ID where applicable.
+        Taken from http://code.djangoproject.com/ticket/4771
+        """
+        attachment = super()._create_attachment(filename, content, mimetype)
+        if filename:
+            mimetype = attachment["Content-Type"]
+            del attachment["Content-Type"]
+            del attachment["Content-Disposition"]
+            attachment.add_header("Content-Disposition", "inline", filename=filename)
+            attachment.add_header("Content-Type", mimetype, name=filename)
+            attachment.add_header("Content-ID", "<%s>" % filename)
+        return attachment
+
+
 class MailService:
     def melding_aangemaakt_email(
-        self, signaal, melding=None, template_stijl="html", verzenden=False
+        self,
+        signaal,
+        melding=None,
+        template_stijl="html",
+        verzenden=False,
+        bestanden=[],
     ):
         if not melding:
             melding = MeldingenService().melding_ophalen_met_signaal_url(
@@ -36,14 +118,12 @@ class MailService:
                 + "' width='300'>"
             )
         onderwerpen_verbose = ", ".join(onderwerpen_list)
-        bijlagen_verbose = ", ".join(bijlagen_list)
         email_context = {
             "melding": melding,
             "begraafplaats": begraafplaats,
             "signaal": signaal,
             "onderwerpen": onderwerpen_verbose,
-            "bijlagen": bijlagen_verbose,
-            "bijlagen_list": bijlagen,
+            "bijlagen": bestanden,
         }
         if begraafplaats.email:
             send_to.append(begraafplaats.email)
@@ -55,10 +135,15 @@ class MailService:
         text_content = text_template.render(email_context)
         html_content = html_template.render(email_context)
         subject = f"Begraafplaats {begraafplaats.naam} - melding aangemaakt"
-        msg = EmailMultiAlternatives(
+        msg = EmailMultiRelated(
             subject, text_content, settings.DEFAULT_FROM_EMAIL, send_to
         )
         msg.attach_alternative(html_content, "text/html")
+
+        for f in bestanden:
+            attachment = default_storage.path(f)
+            print(attachment)
+            msg.attach_related_file(attachment)
 
         if send_to and not settings.DEBUG and verzenden:
             msg.send()
