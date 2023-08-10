11beba5bacbcf5e697590b4f97b297fd5981f8a4
Maurice Guikema <mguikema@gmail.com>
2023-08-10 16:22:27 +0200

    oidc token use basis auth

--- app/apps/authenticatie/auth.py
+++ app/apps/authenticatie/auth.py
@@ -18,3 +18,19 @@ class OIDCAuthenticationBackend(auth.OIDCAuthenticationBackend):
         user.save()
 
         return user
+
+    def get_userinfo(self, access_token, id_token, payload):
+        """Return user details dictionary. The id_token and payload are not used in
+        the default implementation, but may be used when overriding this method"""
+        print(id_token)
+        # print(self.get_payload_data(id_token))
+        print(payload)
+        # user_response = requests.get(
+        #     self.OIDC_OP_USER_ENDPOINT,
+        #     headers={"Authorization": "Bearer {0}".format(access_token)},
+        #     verify=self.get_settings("OIDC_VERIFY_SSL", True),
+        #     timeout=self.get_settings("OIDC_TIMEOUT", None),
+        #     proxies=self.get_settings("OIDC_PROXY", None),
+        # )
+        # user_response.raise_for_status()
+        return payload
